b6c50e7fb90fdc96d7086ec0af5d69b970315834
Luke Sorrenti <lrsorrenti@hotmail.com>
2022-12-11 09:40:23 -0600

    added styling, button hover and focus effect, and added javascript that will stop sounds from playing over each other on clicks

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const sounds = ['applause', 'boo', 'gasp', 
-'tada', 'victory', 'wrong' ];
+'tada', 'victory', 'wrong'];
 
-sounds.forEach(soundName=> {
+
+function stopSongs(){
+    sounds.forEach(soundName =>{
+        const sound = document.getElementById(soundName).play()
+    })
+}
+
+
+sounds.forEach(soundName => {
     const btn = document.createElement('button')
-    btn.classList.add('btn')
-   btn.innerText = soundName
+    btn.classList.add('btn');
+   btn.innerText = soundName;
+
+   btn.addEventListener('click', () => {
+    stopSongs()
+    document.getElementById(soundName).play()
+   });
 
    document.getElementById('button-container').
-   appendChild(btn)
+   appendChild(btn);
 })
+
